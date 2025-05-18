648505a8fe8df84868b720cb5d6cc7bfb753ca74
Arturiooo <arthur-nahushev@mail.ru>
2025-05-18 16:25:20 +0300

    Добавлены эмодзи в ответное сообщение

--- bot/sheduler.py
+++ bot/sheduler.py
@@ -1,48 +1,53 @@
-# from apscheduler.schedulers.asyncio import AsyncIOScheduler
-# from apscheduler.triggers.cron import CronTrigger
-# from aiogram import Bot
-# import asyncio
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+from aiogram import Bot
+from database.db import get_all_user_times_and_cities
+from utils.weather import get_weather_data_by_city
+from utils.weather import get_weather_data_by_city
 
-# from database.db import get_all_user_times_and_cities
-# from utils.weather import get_weather_data_by_city
+scheduler = AsyncIOScheduler()
 
-# scheduler = AsyncIOScheduler()
+async def send_weather_report(bot: Bot, user_id: int, city: str):
+    try:
+        weather = await get_weather_data_by_city(city)
 
+        # Формируем сообщение
+        message = (f"{weather['greeting']}!\n"
+                   f"Погода в городе {weather['city']}\n\n"
+                    "📋 Текущие данные\n"
+                   f"🕒 Местное время: {weather['time']}\n"
+                   f"🌡 Температура: {weather['temp']}°C\n"
+                   f"🌡 Ощущается как: {weather['feels_like']}°C\n"
+                   f"💧 Влажность: {weather['humidity']}%\n"
+                   f"💨 Ветер: {weather['wind']} м/с")
 
-# async def send_weather_report(bot: Bot, user_id: int, city: str):
-#     try:
-#         report = await get_weather_data_by_city(city)
-#         await bot.send_message(user_id, report)
-#     except Exception as e:
-#         print(f"[!] Ошибка при отправке отчета пользователю {user_id}: {e}")
+        await bot.send_message(user_id, message)
 
+    except Exception as e:
+        print(f"[!] Ошибка при отправке отчета пользователю {user_id}: {e}")
 
-# async def schedule_weather_jobs(bot: Bot):
-#     """
-#     Создает задания на отправку прогноза погоды по временам из базы.
-#     Один пользователь может иметь несколько времен (или ни одного).
-#     """
-#     data = get_all_user_times_and_cities()  # [(user_id, city, time), ...]
+def schedule_weather_jobs(bot: Bot):
+    data = get_all_user_times_and_cities() 
 
-#     if not data:
-#         print("[!] Нет данных о времени и городах в базе.")
-#         return
+    if not data:
+        print("[!] Нет данных о времени и городах в базе.")
+        return
 
-#     for user_id, city, time_str in data:
-#         try:
-#             hour, minute = map(int, time_str.split(":"))
-#             trigger = CronTrigger(hour = hour, minute = minute)
+    for user_id, city, time_str in data:
+        try:
+            hour, minute = map(int, time_str.split(":"))
+            trigger = CronTrigger(hour=hour, minute=minute)
 
-#             job_id = f"{user_id}_{city}_{time_str}"
+            job_id = f"{user_id}_{city}_{time_str}"
 
-#             scheduler.add_job(
-#                 lambda bot = bot, user_id=user_id, city=city: asyncio.create_task(
-#                     send_weather_report(bot, user_id, city)
-#                 ),
-#                 trigger=trigger,
-#                 id=job_id,
-#                 replace_existing=True
-#             )
-#             print(f"[+] Задача добавлена: {job_id}")
-#         except Exception as e:
-#             print(f"[!] Ошибка при добавлении задачи {user_id=} {city=} {time_str=}: {e}")
+            scheduler.add_job(
+                send_weather_report,
+                trigger=trigger,
+                args=[bot, user_id, city],
+                id=job_id,
+                replace_existing=True
+            )
+
+            print(f"[+] Задача добавлена: {job_id}")
+        except Exception as e:
+            print(f"[!] Ошибка при добавлении задачи {user_id=} {city=} {time_str=}: {e}")
