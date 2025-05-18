e173505f6f1f0fc604109bf7284ee38f25be1ef2
Arturiooo <arthur-nahushev@mail.ru>
2025-05-18 16:20:36 +0300

    Добавлено обновление задач каждые 30 секунд

--- bot/main.py
+++ bot/main.py
@@ -2,7 +2,7 @@
 import asyncio
 
 #--- Отслеживание состояния --#
-# from sheduler import scheduler, schedule_weather_jobs
+from sheduler import scheduler, schedule_weather_jobs
 from aiogram import Bot, Dispatcher
 
 #--- Файлы с функциями ---#
@@ -13,11 +13,22 @@ import handlers.start_handler as start_handler
 #--- Токен из config ---#
 from config import BOT_TOKEN
 
+# Обновляем задачи каждую минуту
+async def weather_jobs_updater(bot: Bot):
+    while True:
+        schedule_weather_jobs(bot)
+        await asyncio.sleep(30)
 
 #--- Запуск бота ---#
 async def main():
     bot = Bot(BOT_TOKEN)
     dp = Dispatcher()
+
+    # Запускаем планировщик
+    scheduler.start()
+
+    # запускаем автообновление задач
+    asyncio.create_task(weather_jobs_updater(bot))
 
     dp.include_router(notification_handler.time_router)
     dp.include_router(get_weather_handler.get_w_router)
